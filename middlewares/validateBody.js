c374a54bfd7349621986191d51164295ab3014de
vvdiduh <vvdiduh@gmail.com>
2023-06-11 23:26:35 +0300

    зробив динамічне підставлення не вірно заповененого полля при додавані нового контакту

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,8 +3,9 @@ const { HttpError } = require('../helpers');
 const validateBody = schema => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+    const keyError = error.details[0].context.key;
     if (error) {
-      next(HttpError(400, 'missing required name field'));
+      next(HttpError(400, `missing required ${keyError} field`));
     }
     next();
   };
