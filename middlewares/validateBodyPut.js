14db893d06f3d28868af0d33b91d0580f6655373
vvdiduh <vvdiduh@gmail.com>
2023-06-13 22:59:38 +0300

    ну цей раз точно все ок має бути

--- middlewares/validateBodyPut.js
+++ middlewares/validateBodyPut.js
@@ -2,11 +2,17 @@ const { HttpError } = require('../helpers');
 
 const validateBody = schema => {
   const func = (req, res, next) => {
-    const { value } = schema.validate(req.body);  
+    const { value } = schema.validate(req.body);
+    const { error } = schema.validate(req.body);
+
     if (!Object.keys(value).length) {
       next(HttpError(400, 'missing fields'));
+    } else if (error) {
+      const keyError = error.details[0].context.key;
+      next(HttpError(400, `missing required ${keyError} field`));
+    } else {
+      next();
     }
-    next();
   };
   return func;
 };
